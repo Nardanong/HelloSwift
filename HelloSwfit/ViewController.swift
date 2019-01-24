01795c585d3c30cc7a37ca6c11460b7db814f946
akapan <akapan.r@hitachi-tstv.com>
2019-01-24 10:39:58 +0700

    Change Lable Success

--- HelloSwfit/ViewController.swift
+++ HelloSwfit/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    Explicit
+    let myName:String = "Nardanong.s1"
+    
+    
+    @IBOutlet weak var TitleLable: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    } //Main Mothod
+    
+    @IBAction func changeButton(_ sender: UIButton) {
+        
+//        Repeat myName
+        TitleLable.text = myName
+        
+        
+        
     }
+} //Main Class
 
-
-}
 
